fed8009586b5f99da25e229eac1d21eba39bf3ce
vsroy <vishalshillong258@gmail.com>
2019-10-20 11:14:18 +0530

    Adding solution to problem number 168

---
+++ 168_Excel_Sheet.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+
+std::string convertToTitle(int n) 
+{
+	std::string resultString;
+	while (n > 0)
+	{
+		int rem = n % 26;
+		if (rem == 0)
+		{
+			resultString += 'Z';
+			n = (n / 26) - 1;
+		}
+		else
+		{
+			resultString += (rem - 1) + 'A';
+			n = n / 26;
+		}
+	}
+	reverse(resultString.begin(), resultString.end());
+	return resultString;
+}
+
+int main()
+{
+	std::cout << convertToTitle(701);
+	return 0;
+}
