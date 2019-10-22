c248c5ffc785e0a06c47bd0565c00c2baf3e6e87
vsroy <vishalshillong258@gmail.com>
2019-10-22 19:28:12 +0530

    Adding solution to problem number 172

---
+++ 172_Factorial_Trailing_Zeroes.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int trailingZeroes(int n) 
+{
+	int result = 0;
+	while (n > 0)
+	{
+		n /= 5;
+		result += n;
+	}
+	return result;
+}
+
+int main()
+{
+	std::cout << trailingZeroes(5);
+	return 0;
+}
